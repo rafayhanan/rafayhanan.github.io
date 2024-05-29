375d0a02c9f6e024da03fdbe3bb4e59f79f9efaf
rafayhanan <mrafayhanan@gmail.com>
2024-05-30 01:34:53 +0500

    Added mobile media query

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-window.addEventListener('load', function() {
-    var loadingScreen = document.getElementsByClassName('loader');
-    loadingScreen.style.display = 'none';
+document.addEventListener("DOMContentLoaded", function() {
+    const navbarToggle = document.querySelector(".navbar-toggle");
+    const menu = document.querySelector("nav ul");
+  
+    navbarToggle.addEventListener("click", function() {
+      menu.classList.toggle("show");
+      navbarToggle.classList.toggle("change");
+    });
 });
+
