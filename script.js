7354de26b472009fafbf9d0f1a931fe7e830d7f2
rafayhanan <mrafayhanan@gmail.com>
2024-05-30 02:58:22 +0500

    Added form validations

--- script.js
+++ script.js
@@ -21,14 +21,75 @@ document.addEventListener("DOMContentLoaded", function() {
 
 /*Open my email*/
 function openEmail() {
-    const emailAddress = 'your-email@example.com';
-    const subject = 'Subject of the email';
-    const message = 'Your pre-written message goes here.';
+    const emailAddress = 'mrafayhanan@gmail.com';
+    const subject = 'Subject';
+    const message = 'Hello Muhammad!\n';
     
     const mailtoLink = `mailto:${emailAddress}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}`;
     
     window.open(mailtoLink, '_blank');
 }
 
+/*Form validation*/
+const inputs = document.querySelectorAll('.input-field');
+
+// Add event listeners to each input field
+inputs.forEach(input => {
+    input.addEventListener('input', () => {
+        if (input.validity.valid) {
+            input.classList.remove('invalid');
+        } else {
+            input.classList.add('invalid');
+        }
+    });
+});
 
 
+document.addEventListener("DOMContentLoaded", function() {
+    const inputFields = document.querySelectorAll(".input-field");
+  
+    inputFields.forEach(function(input) {
+      input.addEventListener("blur", function() {
+        validateField(input);
+      });
+    });
+  
+    function validateField(input) {
+      if (input.validity.valid) {
+        input.classList.remove("invalid");
+        document.getElementById(input.id + "-error").textContent = "";
+      } else {
+        input.classList.add("invalid");
+        displayErrorMessage(input);
+      }
+    }
+  
+    function displayErrorMessage(input) {
+      let errorMessage = "";
+      if (input.validity.valueMissing) {
+        errorMessage = "This field is required.";
+      } else if (input.validity.typeMismatch) {
+        errorMessage = "Please enter a valid email address.";
+      }
+      document.getElementById(input.id + "-error").textContent = errorMessage;
+    }
+  });
+
+  
+  document.querySelector(".sendMessage-btn").addEventListener("click", function(event) {
+    const inputFields = document.querySelectorAll(".input-field");
+    let isValid = true;
+  
+    inputFields.forEach(function(input) {
+      if (!input.validity.valid) {
+        isValid = false;
+        validateField(input);
+      }
+    });
+  
+    if (!isValid) {
+      event.preventDefault();
+      return false;
+    }
+  });
+  
