4b3a190bab14e813b1d1a9332bb68a1462eba3bc
rafayhanan <mrafayhanan@gmail.com>
2024-05-30 02:27:15 +0500

    Added JS for buttons

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*Toggle Menu Button*/
 document.addEventListener("DOMContentLoaded", function() {
     const navbarToggle = document.querySelector(".navbar-toggle");
     const menu = document.querySelector("nav ul");
@@ -7,4 +8,27 @@ document.addEventListener("DOMContentLoaded", function() {
       navbarToggle.classList.toggle("change");
     });
 });
+
+/*Download Resume Button*/
+document.addEventListener("DOMContentLoaded", function() {
+    const downloadButton = document.querySelector(".red-btn");
+    
+    downloadButton.addEventListener("click", function() {
+        const resumeURL = 'Resume/Resume-Muhammad Rafay.pdf';
+        window.open(resumeURL, '_blank');
+    });
+});
+
+/*Open my email*/
+function openEmail() {
+    const emailAddress = 'your-email@example.com';
+    const subject = 'Subject of the email';
+    const message = 'Your pre-written message goes here.';
+    
+    const mailtoLink = `mailto:${emailAddress}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(message)}`;
+    
+    window.open(mailtoLink, '_blank');
+}
+
+
 
